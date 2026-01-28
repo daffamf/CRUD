431d47d40651192b9625b58c4288be521217bc58
daffamf <daffamuhammadfirdaus5@gmail.com>
2026-01-28 21:05:09 +0700

    finaly apps

--- routes/server.js
+++ routes/server.js
@@ -38,6 +38,51 @@ module.exports = (pool) => {
     }
 });
 
+// PROSES DELETE
+router.get('/delete/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id; // Mengambil ID dari URL
+    
+    // Query untuk menghapus data berdasarkan ID
+    await pool.query('DELETE FROM karyawan WHERE id = $1', [id]);
+    
+    // Setelah dihapus, balik lagi ke halaman utama
+    res.redirect('/');
+  } catch (err) {
+    console.error("Gagal menghapus data:", err);
+    next(err);
+  }
+});
 
+//  Menampilkan Form Edit
+router.get('/edit/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const result = await pool.query('SELECT * FROM karyawan WHERE id = $1', [id]);
+    
+    // Kirim data karyawan y
+    res.render('edit', { item: result.rows[0] });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//  Memproses Update data
+router.post('/edit/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { name, height, weight, birthdate, married } = req.body;
+
+    const sql = `UPDATE karyawan 
+                 SET name = $1, height = $2, weight = $3, birthdate = $4, married = $5 
+                 WHERE id = $6`;
+    
+    await pool.query(sql, [name, height, weight, birthdate, married === 'true', id]);
+    
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
   return router;
 };
